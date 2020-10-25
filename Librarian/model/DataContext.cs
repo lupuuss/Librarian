9af299a7a0b35655df688b2ec799e0d668c1ff40
Damian Baczyński <224250@edu.p.lodz.pl>
2020-10-25 16:07:02 +0100

    Adds DataContext

---
+++ Librarian/model/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Librarian.model.data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Librarian.model
+{
+    class DataContext
+    {
+
+        public List<Customer> customers
+        { get; private set; }
+
+        public Dictionary<Isbn, Book> books
+        { get; private set; }
+
+        public ObservableCollection<Lending> lendings
+        { get; private set; }
+
+        public ObservableCollection<BookCopy> bookCopies
+        { get; private set; }
+
+    }
+}
