add5f5da64180a08fdb955494a1246170ba86bec
Filip.Kowalski195 <Filip.Kowalski195@gmail.com>
2020-10-25 14:11:41 +0100

    Adds Customer data model.

---
+++ Librarian/model/data/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Librarian.model.data
+{
+    public class Customer
+    {
+        public Guid Guid
+        { get; private set; }
+
+        public string Name
+        { get; private set; }
+
+        public string LastName
+        { get; private set; }
+
+        public Customer(string name, string lastName, Guid guid)
+        {
+            Name = name;
+            LastName = lastName;
+            Guid = guid;
+        }
+
+        public Customer(string name, string lastName) : this(name, lastName, Guid.NewGuid()) { }
+    }
+}
