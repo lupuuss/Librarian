d1ac4a803439c49b98e337f04ba28cc4a713fb34
Filip.Kowalski195 <Filip.Kowalski195@gmail.com>
2020-10-25 14:12:22 +0100

    Adds Lending data model

---
+++ Librarian/model/data/Lending.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Librarian.model.data
+{
+    public class ReturnDateBeforeLendingDateException : Exception
+    {
+        public ReturnDateBeforeLendingDateException() 
+            : base("Book copy returning date must be after book was landed.") { }
+    }
+
+    public class Lending
+    {
+        public Guid Guid
+        { get; private set; }
+        public Customer Customer
+        { get; private set; }
+        
+        public BookCopy BookCopy
+        { get; private set; }
+
+        public DateTime LendingDate
+        { get; set; }
+
+        private DateTime? _returnDate = null;
+
+        public DateTime? ReturnDate
+        { 
+            get => _returnDate;
+            set {
+                if (value < LendingDate)
+                {
+                    throw new ReturnDateBeforeLendingDateException();
+                }
+
+                _returnDate = value;
+            } 
+        }
+
+        public Lending(Customer customer, BookCopy bookCopy, Guid guid)
+        {
+            Customer = customer;
+            BookCopy = bookCopy;
+            Guid = guid;
+        }
+
+        public Lending(Customer customer, BookCopy bookCopy) : this(customer, bookCopy, Guid.NewGuid()) { }
+
+        bool IsReturned()
+        {
+            return ReturnDate != null;
+        }
+    }
+}
