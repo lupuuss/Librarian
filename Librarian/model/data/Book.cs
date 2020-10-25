cfca0012d8b75a6be260181b8025e8234cd54a77
Damian Baczyński <224250@edu.p.lodz.pl>
2020-10-25 13:10:12 +0100

    Adds books data model (classes Book and BookCopy)

---
+++ Librarian/model/data/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Librarian
+{
+    public class Isbn : IEquatable<Isbn>
+    {
+        string _value;
+        public Isbn(string isbn)
+        {
+
+            var regex = new Regex(@".*"); // TODO: Isbn Regex 
+
+            if (!regex.Match(isbn).Success)
+            {
+                throw new ArgumentException("Passed value is not a valid ISBN!");
+            }
+
+            _value = isbn;
+        }
+
+        public bool Equals(Isbn other)
+        {
+            return _value == other._value;
+        }
+    }
+
+    public class Book
+    {
+
+        public Isbn Isbn
+        { get; private set; }
+        public string Name
+        { get; private set; }
+        public string Author
+        { get; private set; }
+
+        public Book(string name, Isbn isbn)
+        {
+            Name = name;
+            Isbn = isbn;
+        }
+    }
+}
---
+++ Librarian/model/data/BookCopy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Librarian.model.data
+{
+    public class BookCopy
+    {
+        /// <summary>
+        /// State value represents percentage of a base price that need to be paid in case of customer damage.
+        /// Dameged has -1 as its represent damaged book that is already paid.
+        /// </summary>
+        public enum States
+        {
+            /// <summary>
+            /// Damaged by the customer e.g. ripped page.
+            /// </summary>
+            Damaged = -1,
+            /// <summary>
+            /// Needs replacement, but not because of single customer damage, but because of long time exploatation
+            /// </summary>
+            NeedReplacement = 0,
+            /// <summary>
+            /// Have significant traces of usage, but still can be used. May require replacement in the future.
+            /// </summary>
+            Used = 50,
+            /// <summary>
+            /// Have minor traces of usage.
+            /// </summary>
+            Good = 80,
+            /// <summary>
+            /// Brand new. No signs of usage.
+            /// </summary>
+            New = 100
+        }
+
+        public States State
+        { get; set; }
+
+        public Guid Guid
+        { get; private set; }
+
+        public Book Book
+        { get; private set; }
+
+        public double BasePrice
+        { get; private set; }
+
+        public double Price => BasePrice * (double) State / 100.0;
+
+        public BookCopy(Guid guid, Book book, States state, double basePrice)
+        {
+            Guid = guid;
+            Book = book;
+            State = state;
+            BasePrice = basePrice;
+        }
+    }
+}
