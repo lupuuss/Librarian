f7b66830eb45f061e9511f79f9b7da19dfb9bb24
Damian Baczyński <224250@edu.p.lodz.pl>
2020-10-25 19:12:15 +0100

    Adds IDataFiller

---
+++ Librarian/model/filler/IDataFiller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Librarian.model.filler
+{
+    interface IDataFiller
+    {
+        void Fill(DataContext context);
+    }
+}
