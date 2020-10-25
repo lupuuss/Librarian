d1b17c879a30b1eab5ac35588df66696f4001e0f
Damian Baczyński <224250@edu.p.lodz.pl>
2020-10-25 19:11:51 +0100

    Adds abstraction layer over file operations (ILocalStorage and LocalStorage)

---
+++ Librarian/env/ILocalStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Librarian.env
+{
+    public class DirectoryNotCreatedException : Exception
+    {
+        public DirectoryNotCreatedException(Exception cause) 
+            : base("Directory could not be created!", cause) { }
+    }
+
+    public interface ILocalStorage
+    {
+        bool DirectoryExists(string dir);
+
+        void CreateDirectory(string dir);
+    }
+}
