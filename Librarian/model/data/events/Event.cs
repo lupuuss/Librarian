a51ec7dfc6a28f95002b099255a60e2b1a0b22df
Filip.Kowalski195 <Filip.Kowalski195@gmail.com>
2020-11-06 22:11:44 +0100

    Adds exceptions to Event.

--- Librarian/model/data/events/Event.cs
+++ Librarian/model/data/events/Event.cs
@@ -3,6 +3,21 @@
 namespace Librarian.model.data.events
 {
 
+    public class EventException : Exception
+    {
+        public EventException(string message) : base(message) { }
+    }
+
+    public class InvalidEventException : EventException
+    {
+        public InvalidEventException(string cause) : base("Invalid event! Cause: " + cause) { }
+    }
+
+    public class UnrecognizedEventException : EventException 
+    {
+        public UnrecognizedEventException() : base("Unrecognized Event implementation!") { }
+    }
+
     public abstract class Event
     {
         public DateTime Date
