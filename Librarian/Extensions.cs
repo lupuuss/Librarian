04ee8f115894dba861bb9537842c93890e2a811e
Damian Baczyński <224250@edu.p.lodz.pl>
2020-11-07 23:08:49 +0100

    Adds RemoveAll extension to ICollection<T>

---
+++ Librarian/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Librarian
+{
+    public static class Extensions
+    {
+        public static void RemoveAll<T>(this ICollection<T> list, IEnumerable<T> toRemove)
+        {
+            foreach (var element in toRemove)
+            {
+                list.Remove(element);
+            }
+        }
+    }
+}
