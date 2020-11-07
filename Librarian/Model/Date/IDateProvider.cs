dd2aba12d61a24ccdcb7c6c2c846ac97a157314b
Filip.Kowalski195 <Filip.Kowalski195@gmail.com>
2020-11-07 18:38:45 +0100

    Adds DateProvider.

---
+++ Librarian/Model/Date/IDateProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Librarian.Model.Date
+{
+    public interface IDateProvider
+    {
+        DateTime Now();
+    }
+}
---
+++ Librarian/Model/Date/SystemDateProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Librarian.Model.Date
+{
+    public class SystemDateProvider : IDateProvider
+    {
+        public DateTime Now()
+        {
+            return DateTime.Now; 
+        }
+    }
+}
