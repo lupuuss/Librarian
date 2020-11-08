abf6c01013c369566dc762dc9b815187d34454c4
Damian Baczyński <224250@edu.p.lodz.pl>
2020-11-08 13:14:48 +0100

    Adds Utils.AreEqual to compare doubles with tolerance

---
+++ LibrarianTests/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarianTests
+{
+    public static class Utils
+    {
+        public static bool AreEqual(double x, double y, double tolerance)
+        {
+            double difference = Math.Abs(x * tolerance);
+
+            return Math.Abs(x - y) <= difference;
+        }
+    }
+}
