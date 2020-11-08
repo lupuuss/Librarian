249555acbc5bf7e1b7dcbbf83b632978640c583b
Damian Baczyński <224250@edu.p.lodz.pl>
2020-11-08 18:52:05 +0100

    Adds AddPayment and CheckBalance to DataServices (with tests)

--- Librarian/Logic/IDataService.cs
+++ Librarian/Logic/IDataService.cs
@@ -19,6 +19,8 @@ namespace Librarian.Logic
         Dictionary<Book, int> GetBooks();
         IEnumerable<BookCopy> GetAllCopies(Isbn isbn);
         void AddBook(Isbn isbn, string name, string author);
-        void AddBookCopy(Book book, BookCopy.States state, double price);  
+        void AddBookCopy(Book book, BookCopy.States state, double price);
+        void AddPayment(Customer customer, double amount);
+        double CheckBalance(Customer customer);
     }
 }
